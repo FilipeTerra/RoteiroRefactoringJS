d8ea96c46f0bc50736dcc1cd11c433c062ccb990
filipecfundep <filipecaldeira@fundep.com.br>
2025-11-09 22:06:00 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -13,16 +13,15 @@ function gerarFaturaStr (fatura, pecas) {
                             minimumFractionDigits: 2 }).format;
   
     for (let apre of fatura.apresentacoes) {
-      const peca = getPeca(apre);
-      let total = calcularTotalApresentacao(apre, peca);
+      let total = calcularTotalApresentacao(apre);
 
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
 
